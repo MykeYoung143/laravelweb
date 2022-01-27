671fe8af4a4bcb6fe5dee604abfd063bab510a35
unknown <michaelalex997@gmail.com>
2022-01-27 10:32:20 +0700

    ui ux login page

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
 
-<div class="row justify-content-center">
+<!-- <div class="row justify-content-center">
     <div class="col-lg-4">
 
       @if(session()->has('success'))
@@ -45,6 +45,62 @@
         </main>
 
     </div>
-</div>
+</div> -->
+
+<div class="page-content page-auth">
+    <div class="section-store-auth" data-aos="fade-up">
+      <div class="container">
+        <div class="row align-items-center row-login">
+          <div class="col-lg-6 text-center">
+            <img
+              src="/img/login-placeholder.png"
+              alt=""
+              class="w-50 mb-4 mb-lg-none"
+            />
+          </div>
+          <div class="col-lg-5">
+            <h2>Care Your Pet <br />With Us</h2>
+            @if(session()->has('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+              {{ session('success') }}
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+              </button>
+            </div>
+            @endif
+
+            @if(session()->has('loginError'))
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+              {{ session('loginError') }}
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+              </button>
+            </div>
+            @endif
+            <form action="/login" method="post" class="mt-3">
+              @csrf
+              <div class="form-group">
+                <label for="email">Email Address</label>
+                <input type="email" name="email" class="form-control w-75 @error('email') is-invalid @enderror"  id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
+                @error('email')
+                  <div class="invalid-feedback">
+                    {{ $message }}
+                  </div>
+                @enderror
+              </div>
+              <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control w-75" id="password" placeholder="Password" required>
+              </div>
+              <button class="btn btn-primary btn-block w-75" type="submit"
+                >Sign In</button
+              >
+              <a href="/register" class="btn btn-light btn-block w-75"
+                >Sign Up</a
+              >
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
 @endsection
