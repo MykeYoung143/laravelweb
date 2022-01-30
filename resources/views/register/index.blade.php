49dce740e97e23d27315cfc2fce7ad5a45445c68
unknown <michaelalex997@gmail.com>
2022-01-30 11:28:53 +0700

    renew backup + register

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -1,43 +1,54 @@
 @extends('layouts.main')
 
 @section('container')
-
-<div class="row justify-content-center">
-    <div class="col-lg-4">
-        <main class="form-registration">
-            <h1 class="h3 mb-3 fw-normal text-center">Registration Form</h1>
+<div class="page-content page-auth">
+    <div class="section-store-auth" data-aos="fade-up">
+      <div class="container">
+        <div class="row align-items-center row-login">
+          <div class="col-lg-6 text-center">
+            <img
+              src="/img/register.png"
+              alt=""
+              class="w-50 mb-4 mb-lg-none"
+            />
+          </div>
+          <div class="col-lg-5">
+            <h2>Give the best for your pets<br />with Us</h2>
           <form action="/register" method="post">
             @csrf
-            <div class="form-floating">
-              <input type="text" name="name" class="form-control rounder-top @error('name') is-invalid @enderror" id="name" placeholder="Name" required value="{{ old('name') }}">
-              <label for="name">Name</label>
+            <div class="form-group">
+			      <label for="name">Name</label>
+              <input type="text" name="name" class="form-control w-75 @error('name') is-invalid @enderror" id="name" placeholder="Name" required value="{{ old('name') }}">
+              
               @error ('name')
               <div class="invalid-feedback">
                 {{ $message }}
               </div>
               @enderror
             </div>
-            <div class="form-floating">
-              <input type="text" name="username" class="form-control @error('username') is-invalid @enderror" id="username" placeholder="Username" required value="{{ old('username') }}">
-              <label for="username">Username</label>
+            <div class="form-group">
+				<label for="username">Username</label>
+              <input type="text" name="username" class="form-control w-75 @error('username') is-invalid @enderror" id="username" placeholder="Username" required value="{{ old('username') }}">
+              
               @error ('username')
               <div class="invalid-feedback">
                 {{ $message }}
               </div>
               @enderror
             </div>
-            <div class="form-floating">
-              <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" required value="{{ old('email') }}">
-              <label for="floatingInput">Email address</label>
+            <div class="form-group">
+				<label for="floatingInput">Email address</label>
+              <input type="email" name="email" class="form-control w-75 @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" required value="{{ old('email') }}">
+              
               @error ('email')
               <div class="invalid-feedback">
                 {{ $message }}
               </div>
               @enderror
             </div>
-            <div class="form-floating">
-              <input type="password" name ="password" class="form-control rounded-bottom @error('password') is-invalid @enderror" id="floatingPassword" placeholder="Password" required>
-              <label for="floatingPassword">Password</label>
+            <div class="form-group">
+				<label for="floatingPassword">Password</label>
+              <input type="password" name ="password" class="form-control w-75 @error('password') is-invalid @enderror" id="floatingPassword" placeholder="Password" required>
               @error ('password')
               <div class="invalid-feedback">
                 {{ $message }}
@@ -45,12 +56,15 @@
               @enderror
             </div>
 
-            <button class="w-100 btn btn-lg btn-primary mt-4" type="submit">Register</button>
+            <button class=" btn btn-block w-75 mt-4" type="submit">Register</button>
+			   <a href="/login" class="btn btn-block w-75" id="btn-3"
+                >Already Registered? Login</a>
           </form>
-            <small class="d-block text-center mt-3">Already Registered? <a href="/login">Login</small>
-        </main>
 
     </div>
 </div>
+		   </div>
+    </div>
+  </div>
 
 @endsection
