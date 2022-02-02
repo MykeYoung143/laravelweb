6cd593141e9f633536f2273f019bfd77a5b0a682
unknown <michaelalex997@gmail.com>
2022-02-02 17:24:50 +0700

    card fixup and icons

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,113 +12,128 @@
                 <div class="col-lg-6 col-md-12" data-aos="fade-up" data-aos-duration="2000">
                     <div class="text">
                         <h2>Miarao is</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum nisi, facilisis neque massa montes, tellus iaculis ut senectus.</p>
+                        <p>Sebuah aplikasi yang dibuat untuk memenuhi kebutuhan hewan dari mulai medis, makanan pokok dan sebagainya untuk mendukung kelangsungan
+                            dan kefektifitasan untuk memelihara mereka.
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
+</section>
+
+<section class="about-detail">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12">
+                    <div class="text" data-aos="fade-up">
+                        <h2>What is Miarao exactly ?</h2>
+                        <p>Sebuah aplikasi yang dibuat berdasarkan pengalaman pribadi memelihara  binatang selama lebih dari 10 tahun, dimana tidak ada apps yang bisa mendukung kelangsungan dan kefektifitasan untuk memelihara mereka. Khususnya untuk sistem digitalisasi yang ada di sektor medis. Selain itu kesulitan para pemilik untuk mendapatkan info  dan saling bertukar pikiran. Kepada para sektor pendukung , kami juga ingin meningkatkan industri pet economy yang akan meningkat tiap tahunnya.</p>
+                    </div>
+                    <br>
+                    <div class="text" data-aos="fade-up">
+                    <h2>For Whom Miarao is made for?</h2>
+                    <p>Ditujukan kepada orang - orang pecinta hewan yang memberikan diri mereka predikat sebagai orang tua bagi hewan peliharaan mereka. Selain itu juga kepada semua komponen pendukungnya. </p>
+                </div>
+                <br>
+                <div class="text" data-aos="fade-up">
+                    <h2>Why the name “Miarao” ?</h2>
+                    <p>Miarao berasal dari kata memelihara  yang  kemudian kami singkat menjadi “miara” karena sesuai dengan pelafalan di sekitar kami. Penambahan kata “o” di belakang sendiri mengarahkan kepada bahasa Jawa yang memiliki arti mengajak. Jadi kami ingin mengajak pada pencinta hewan untuk memelihara dengan memperlakukan mereka sebagai bagian dari keluarga kita.</p>
+                </div>
+                <br>
+                <div class="text" data-aos="fade-up">
+                    <h2>How will Miarao be useful ?</h2>
+                    <p>Untuk Pet owner = mampu memberikan satu wadah yang lengkap dimana mereka bisa meningkatkan kualitas hidup hewan kesayangan mereka. Dimulai dari kesehatan, sosial dan kelangsungan hidup mereka. <br> Untuk komponen pendukung = mampu menaikkan brand awarness dan penjualan mereka, serta membuka peluang untuk bisnis yang lain. </p>
+                </div>
+            </div>
+            </div>
+        </div>
     </section>
+
 
 <h2 class="team-title" data-aos="fade-down">The Team</h2>
     <section class="containers">
         <div class="col-lg-12">
             <div class="container">
         <ul id="autoWidth" class="cs-hidden">
-            <li class="item-a">
+           <li class="item-a">
                 <div class="cards" data-aos="zoom-in">
                     <div class="cards-img">
-                        <img src="/img/team/Samuel.jpg" alt="">
+                        <img src="/img/team/cat.png" alt="">
                     </div>
                     <div class="cards-info">
-                        <h3>Samuel Berrit</h3>
-                        <span><i class="fas fa-code"></i> Project Owner</span>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi commodi quis consectetur at autem perspiciatis, nostrum iure officiis sint, quaerat animi tempore quia aperiam maiores? Ea sed repudiandae maiores vel.</p>
-                        <div class="icons">
-                            <a href="#" class="fab fa-facebook"></a>
-                            <a href="#" class="fab fa-twitter"></a>
-                            <a href="#" class="fab fa-linkedin"></a>
-                            <a href="#" class="fab fa-instagram"></a>
-                        </div>
+                        <h3>Vina Pohan</h3>
+                        <span>Inspirator</span>
                     </div>
                 </div>
             </li>
             <li class="item-b">
                 <div class="cards" data-aos="zoom-in" data-aos-delay="200">
                     <div class="cards-img">
-                        <img src="/img/team/Vina.jpg" alt="">
+                        <img src="/img/team/lion.png" alt="">
                     </div>
                     <div class="cards-info">
-                        <h3>Vina Phusphitasari</h3>
-                        <span><i class="fas fa-code"></i> Project Owner</span>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi commodi quis consectetur at autem perspiciatis, nostrum iure officiis sint, quaerat animi tempore quia aperiam maiores? Ea sed repudiandae maiores vel.</p>
-                        <div class="icons">
-                            <a href="#" class="fab fa-facebook"></a>
-                            <a href="#" class="fab fa-twitter"></a>
-                            <a href="#" class="fab fa-linkedin"></a>
-                            <a href="#" class="fab fa-instagram"></a>
-                        </div>
+                        <h3>Samuel Berrit Olam</h3>
+                        <span>Project Owner</span>
                     </div>
                 </div>
             </li>
             <li class="item-c">
                 <div class="cards" data-aos="zoom-in" data-aos-delay="400">
                     <div class="cards-img">
-                        <img src="/img/team/Rama.jpg" alt="">
+                        <img src="/img/team/dog.png" alt="">
                     </div>
                     <div class="cards-info">
-                        <h3>Rahadian Maulana</h3>
-                        <span><i class="fas fa-code"></i> UI/UX Designer</span>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi commodi quis consectetur at autem perspiciatis, nostrum iure officiis sint, quaerat animi tempore quia aperiam maiores? Ea sed repudiandae maiores vel.</p>
-                        <div class="icons">
-                            <a href="#" class="fab fa-facebook"></a>
-                            <a href="#" class="fab fa-twitter"></a>
-                            <a href="#" class="fab fa-linkedin"></a>
-                            <a href="#" class="fab fa-instagram"></a>
-                        </div>
+                        <h3>Jake Carlo Warouw</h3>
+                        <span>Co-owner</span>
                     </div>
                 </div>
             </li>
             <li class="item-d">
                 <div class="cards" data-aos="zoom-in" data-aos-delay="600">
                     <div class="cards-img">
-                        <img src="/img/team/Dicky.jpg" alt="">
+                        <img src="/img/team/owl.png" alt="">
                     </div>
                     <div class="cards-info">
-                        <h3>Dicky Haruman</h3>
-                        <span><i class="fas fa-code"></i> Front-End Developer</span>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi commodi quis consectetur at autem perspiciatis, nostrum iure officiis sint, quaerat animi tempore quia aperiam maiores? Ea sed repudiandae maiores vel.</p>
-                        <div class="icons">
-                            <a href="#" class="fab fa-facebook"></a>
-                            <a href="#" class="fab fa-twitter"></a>
-                            <a href="#" class="fab fa-linkedin"></a>
-                            <a href="#" class="fab fa-instagram"></a>
-                        </div>
+                        <h3>Evan Afrianus</h3>
+                        <span>Social Media Creator</span>
                     </div>
                 </div>
             </li>
             <li class="item-e">
                 <div class="cards" data-aos="zoom-in" data-aos-delay="800">
                     <div class="cards-img">
-                        <img src="/img/team/Alex.jpg" alt="">
+                        <img src="/img/team/koala.png" alt="">
+                    </div>
+                    <div class="cards-info">
+                        <h3>Rahadian Maulana</h3>
+                        <span>UI/UX Designer</span>
+                    </div>
+                </div>
+            </li>
+            <li class="item-f">
+                <div class="cards" data-aos="zoom-in" data-aos-delay="800">
+                    <div class="cards-img">
+                        <img src="/img/team/fox.png" alt="">
+                    </div>
+                    <div class="cards-info">
+                        <h3>Dicky Haruman</h3>
+                        <span>Front-End Developer</span>
+                    </div>
+                </div>
+            </li>
+            <li class="item-g">
+                <div class="cards" data-aos="zoom-in" data-aos-delay="800">
+                    <div class="cards-img">
+                        <img src="/img/team/hen.png" alt="">
                     </div>
                     <div class="cards-info">
                         <h3>Michael Alex</h3>
-                        <span><i class="fas fa-code"></i> Back-End Developer</span>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi commodi quis consectetur at autem perspiciatis, nostrum iure officiis sint, quaerat animi tempore quia aperiam maiores? Ea sed repudiandae maiores vel.</p>
-                        <div class="icons">
-                            <a href="#" class="fab fa-facebook"></a>
-                            <a href="#" class="fab fa-twitter"></a>
-                            <a href="#" class="fab fa-linkedin"></a>
-                            <a href="#" class="fab fa-instagram"></a>
-                        </div>
+                        <span>Back-End Developer</span>
                     </div>
                 </div>
             </li>
           </ul>
-        
         </div>
-        
-        </div>
-        
-    </section>
+    </div>
+</section>
 @endsection
--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -1,5 +1,5 @@
 <header class="sticky-top">
-<nav class="navbar navbar-expand-lg" data-aos="fade-down">
+<nav class="navbar navbar-expand-lg" >
   <div class="container-fluid">
     <a class="navbar-brand" href="/"><img src="/img/Starter/Logo-Navi.svg" alt=""></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
